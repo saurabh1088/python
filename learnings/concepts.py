c5d3db78f05f9ad1bd0a1e1705ef0c646972b6f3
Saurabh Verma <saurabh1088@gmail.com>
2024-10-23 22:13:40 +0530

    Created module concepts to try python concepts.

---
+++ learnings/concepts.py
@@ -0,0 +1,9 @@
+def example_dynamic_typing_in_python():
+    x = 10
+    print(f"x is {x} and type of x is {type(x)}")
+    
+    x = 1.5
+    print(f"x is {x} and type of x is {type(x)}")
+
+    x = "string"
+    print(f"x is {x} and type of x is {type(x)}")
--- learnings/main.py
+++ learnings/main.py
@@ -1,6 +1,8 @@
+import concepts
 
 def main():
     print("This is entry point")
+    concepts.example_dynamic_typing_in_python()
 
 if __name__ == '__main__':
     main()
