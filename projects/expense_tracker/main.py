6311d2f12041f1178420e19b218693107551f504
Saurabh Verma <saurabh1088@gmail.com>
2024-10-24 22:19:12 +0530

    Added placeholder project for expense tracker and updated gitignore to remove .DS_Store files from commit.

---
+++ projects/expense_tracker/main.py
@@ -0,0 +1,20 @@
+# Project Requirements:
+# User Stories:
+
+# As a user, I can add a new expense with details such as the amount, category (e.g., Food, Transport), and description.
+# As a user, I can view all my expenses in a list format.
+# As a user, I can delete an expense by its ID.
+# As a user, I can generate a report that summarizes the total amount spent in each category.
+# As a user, I can save my expenses to a file and load them when the application starts.
+# As a user, I can edit an existing expense (change amount, category, or description).
+# Main Features:
+
+# Add, view, delete, edit, and categorize expenses.
+# Data persistence with JSON.
+# Simple report generation based on expense categories.
+
+def main():
+    print("This is entry point for expense tracker app")
+
+if __name__ == '__main__':
+    main()
