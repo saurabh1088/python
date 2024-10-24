8b0803fee52b3c46f3cda1889b37014cf4fd63ad
Saurabh Verma <saurabh1088@gmail.com>
2024-10-24 21:43:36 +0530

    Added example for class type properties.

--- learnings/concepts.py
+++ learnings/concepts.py
@@ -5,14 +5,21 @@ class SampleClassDeclaration:
         print("Initialising class SampleClassDeclaration...")
         pass
 
-class SampleClassWithProperties:
+class SampleClassWithInstanceProperties:
 
     def __init__(self, property):
-        print("Initialising class SampleClassWithProperties...")
+        print("Initialising class SampleClassWithInstanceProperties...")
         self.property = property
 
     def __str__(self):
-        return f"This SampleClassWithProperties instance property's value is {self.property}\n- Type of property is {type(self.property)}"
+        return f"This SampleClassWithInstanceProperties instance property's value is {self.property}\n- Type of property is {type(self.property)}"
+
+class SampleClassWithTypeProperties:
+
+    typeProperty = "value_for_type_property"
+
+    def __init__(self):
+        pass
 
 def example_dynamic_typing_in_python():
     x = 10
@@ -24,8 +31,26 @@ def example_dynamic_typing_in_python():
     x = "string"
     print(f"x is {x} and type of x is {type(x)}")
 
-def example_create_object_for_sample_class_with_properties():
-    objectOne = SampleClassWithProperties(property="string")
-    objectTwo = SampleClassWithProperties(property=100)
+def example_create_object_for_sample_class_with_instance_properties():
+    objectOne = SampleClassWithInstanceProperties(property="string")
+    objectTwo = SampleClassWithInstanceProperties(property=100)
     print(objectOne)
     print(objectTwo)
+
+def example_create_object_for_sample_class_with_type_properties():
+    objectOne = SampleClassWithTypeProperties()
+    objectTwo = SampleClassWithTypeProperties()
+    objectThree = SampleClassWithTypeProperties()
+
+    print("Initial value for type property across multiple instances")
+    print(f"Object 1 : {objectOne.typeProperty}")
+    print(f"Object 2 : {objectOne.typeProperty}")
+    print(f"Object 3 : {objectOne.typeProperty}")
+
+    print("Updating type property value using objectOne...")
+    print("Update finished, printing values again...")
+    objectOne.typeProperty = "updated_type_proprty_value"
+
+    print(f"Object 1 : {objectOne.typeProperty}")
+    print(f"Object 2 : {objectOne.typeProperty}")
+    print(f"Object 3 : {objectOne.typeProperty}")
--- learnings/main.py
+++ learnings/main.py
@@ -4,7 +4,8 @@ def main():
     print("This is entry point")
     concepts.example_dynamic_typing_in_python()
     object = concepts.SampleClassDeclaration()
-    concepts.example_create_object_for_sample_class_with_properties()
+    concepts.example_create_object_for_sample_class_with_instance_properties()
+    concepts.example_create_object_for_sample_class_with_type_properties()
 
 if __name__ == '__main__':
     main()
