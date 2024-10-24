88537b6360c83cd78f0bae71f88fa105a5f25940
Saurabh Verma <saurabh1088@gmail.com>
2024-10-24 20:11:36 +0530

    Added examples around classes and class initialisation.

--- learnings/concepts.py
+++ learnings/concepts.py
@@ -1,3 +1,19 @@
+# Simple class with no properties
+class SampleClassDeclaration:
+
+    def __init__(self):
+        print("Initialising class SampleClassDeclaration...")
+        pass
+
+class SampleClassWithProperties:
+
+    def __init__(self, property):
+        print("Initialising class SampleClassWithProperties...")
+        self.property = property
+
+    def __str__(self):
+        return f"This SampleClassWithProperties instance property's value is {self.property}\n- Type of property is {type(self.property)}"
+
 def example_dynamic_typing_in_python():
     x = 10
     print(f"x is {x} and type of x is {type(x)}")
@@ -7,3 +23,9 @@ def example_dynamic_typing_in_python():
 
     x = "string"
     print(f"x is {x} and type of x is {type(x)}")
+
+def example_create_object_for_sample_class_with_properties():
+    objectOne = SampleClassWithProperties(property="string")
+    objectTwo = SampleClassWithProperties(property=100)
+    print(objectOne)
+    print(objectTwo)
--- learnings/main.py
+++ learnings/main.py
@@ -3,6 +3,8 @@ import concepts
 def main():
     print("This is entry point")
     concepts.example_dynamic_typing_in_python()
+    object = concepts.SampleClassDeclaration()
+    concepts.example_create_object_for_sample_class_with_properties()
 
 if __name__ == '__main__':
     main()
