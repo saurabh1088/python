cb47a075a822426ad9791246bd1d3f78c861347b
Saurabh Verma <saurabh1088@gmail.com>
2024-12-20 22:00:22 +0530

    Added push pop for stack data structure.

--- projects/data_structures/stack.py
+++ projects/data_structures/stack.py
@@ -3,3 +3,13 @@ class Stack:
     def __init__(self):
         # Initialize the stack with empty list
         self.stack = []
+
+    def push(self, item):
+        # Append the item to the stack
+        self.stack.append(item)
+
+    def pop(self):
+        # Pop the last item from the stack
+        return self.stack.pop()
+    
+    
