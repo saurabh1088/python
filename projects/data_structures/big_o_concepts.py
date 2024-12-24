cb650d520ad47d8cee2870080e8fe35076f0d38c
Saurabh Verma <saurabh1088@gmail.com>
2024-12-24 07:08:22 +0530

    Added file for big O notation examples.

---
+++ projects/data_structures/big_o_concepts.py
@@ -0,0 +1,16 @@
+def example_one(n):
+    for i in range(n):
+        print(i)
+
+def example_two(n):
+    for i in range(n):
+        print(i)
+    for j in range(n):
+        print(j)
+
+def example_three(n):
+    for i in range(n):
+        print(i)
+        for j in range(n):
+            print(i,j)
+
