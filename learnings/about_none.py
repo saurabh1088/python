ff4ee1fc5656517ad1176f3f67ad235965710fef
Saurabh Verma <saurabh1088@gmail.com>
2024-12-24 05:59:02 +0530

    Replaced print statements with loggers.

--- learnings/about_none.py
+++ learnings/about_none.py
@@ -1,5 +1,17 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
 def type_of_none():
-    return print(type(None))
+    logging.info('This is about_none.type_of_none()')
+    logging.info(f'Type of None is {type(None)}')
 
 def singleton_nature_of_none():
     objectOne = None
@@ -8,30 +20,33 @@ def singleton_nature_of_none():
     # The is keyword is used to test if two variables refer to the same object.
     # is operator checks for identity, meaning it determines whether two variables point to the same object in memory.
     # The output of the below print statement will be True, because None is a singleton.
-    print(objectOne is objectTwo is objectThree)
+    assert objectOne is objectTwo is objectThree
+    logging.info(f'objectOne is objectTwo is objectThree: {objectOne is objectTwo is objectThree}')
 
 def result_type_of_functions_without_any_return():
     # The return value of a function without a return statement is None.
     # The output of the below print statement will be None.
     output = sample_function_not_returning_any_value()
+    assert output is None
     if output is None:
-        print("Output of sample_function_not_returning_any_value() is None")
-        print(type(output))
+        logging.info("Output of sample_function_not_returning_any_value() is None")
+        logging.info(f'Type of output is {type(output)}')
 
 def usage_as_variable_initialisation():
     # None can be used to initialise a variable. Here object is initialised with None till it is assigned a value.
     object = None
-    print(object)
-    print('Assigning value to object...')
+    logging.info(f'Value of object at beginning is {object}')
+    logging.info('Assigning value to object...')
     object = 10
-    print(f'Value of object is {object}')
+    logging.info('Value assigned to object')
+    logging.info(f'Value of object is {object}')
 
 def usage_as_in_boolean_context():
     # None is considered as False in a boolean context.
     if None:
-        print("None is True")
+        logging.info("None is True")
     else:
-        print("None is False")
+        logging.info("None is False")
 
 def sample_function_not_returning_any_value():
     pass
