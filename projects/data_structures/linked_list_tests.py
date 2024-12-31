c2b5e0f11b3d77774e3d6ef08450b2bf92532986
Saurabh Verma <saurabh1088@gmail.com>
2024-12-31 17:38:53 +0530

    Added unit tests for append method.

--- projects/data_structures/linked_list_tests.py
+++ projects/data_structures/linked_list_tests.py
@@ -16,6 +16,34 @@ class TestLinkedList(unittest.TestCase):
         ll = linked_list.LinkedList(10)
         self.assertIs(ll.head, ll.tail)
 
+    # Test case for append method of LinkedList class checks new node is added to linked list
+    def test_append_method_adds_new_node_to_linked_list(self):
+        ll = linked_list.LinkedList(5)
+        ll.append(10)
+        self.assertEqual(ll.head.value, 5)
+        self.assertEqual(ll.tail.value, 10)
+        self.assertEqual(ll.head.next.value, 10)
+        self.assertEqual(ll.length, 2)
+
+    # Test case for append method of LinkedList class checks new node is added to linked list when head is None
+    def test_append_method_adds_new_node_to_linked_list_when_head_is_None(self):
+        # Create a linked list, then set head and tail to None and length to 0
+        ll = linked_list.LinkedList(5)
+        ll.head = None
+        ll.tail = None
+        ll.length = 0
+
+        # Assert list is empty
+        self.assertEqual(ll.head, None)
+        self.assertEqual(ll.tail, None)
+        self.assertEqual(ll.length, 0)
+
+        # Append a new node
+        ll.append(10)
+        self.assertEqual(ll.head.value, 10)
+        self.assertEqual(ll.tail.value, 10)
+        self.assertEqual(ll.length, 1)
+
 
 if __name__ == "__main__":
     unittest.main()
