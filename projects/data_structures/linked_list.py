7027e20bf0f60c6b3f381f4c89cd12e54be79be8
Saurabh Verma <saurabh1088@gmail.com>
2024-12-31 17:27:21 +0530

    Added method for appending to linked list.

--- projects/data_structures/linked_list.py
+++ projects/data_structures/linked_list.py
@@ -10,14 +10,31 @@ logging.basicConfig(
 )
 
 class Node:
+
     def __init__(self, value):
         self.value = value
         self.next = None
 
 class LinkedList:
+
     def __init__(self, value):
         newNode = Node(value)
         self.head = newNode
         self.tail = newNode
+        self.length = 1
+
+    def append(self, value):
+        # Create a new node
+        newNode = Node(value)
+
+        # Check if the linked list is empty
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+            return
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+        
         self.length = 1
 
