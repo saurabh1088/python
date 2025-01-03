a2943515c1337aa565688a9e72cfafb1d5b20a24
Saurabh Verma <saurabh1088@gmail.com>
2025-01-03 20:46:05 +0530

    Added unit tests for insert method.

--- projects/data_structures/linked_list_tests.py
+++ projects/data_structures/linked_list_tests.py
@@ -147,6 +147,45 @@ class TestLinkedList(unittest.TestCase):
         ll.append(15)
         self.assertEqual(ll.get(0).value, 5)
 
+    # Test case for insert method of LinkedList class when index is out of range
+    def test_insert_method_when_index_is_out_of_range(self):
+        ll = linked_list.LinkedList(5)
+        ll.append(10)
+        ll.append(15)
+        self.assertEqual(ll.insert(20, 4), False)
+
+    # Test case for insert method of LinkedList class when list has one node and index is 0
+    def test_insert_method_when_linked_list_has_one_node_and_index_is_zero(self):
+        ll = linked_list.LinkedList(5)
+        ll.insert(10, 0)
+        self.assertEqual(ll.head.value, 10)
+        self.assertEqual(ll.tail.value, 5)
+        self.assertEqual(ll.length, 2)
+
+    # Test case for insert method of LinkedList class when list has multiple nodes
+    def test_insert_method_when_linked_list_has_multiple_nodes(self):
+        ll = linked_list.LinkedList(5)
+        ll.append(10)
+        ll.append(15)
+        ll.append(20)
+        ll.insert(0, 2)
+        self.assertEqual(ll.head.value, 5)
+        self.assertEqual(ll.tail.value, 20)
+        self.assertEqual(ll.length, 5)
+        self.assertEqual(ll.get(2).value, 0)
+
+    # Test case for insert method of LinkedList class when list has multiple nodes and index is end of the list
+    def test_insert_method_when_linked_list_has_multiple_nodes_and_index_is_end_of_list(self):
+        ll = linked_list.LinkedList(5)
+        ll.append(10)
+        ll.append(15)
+        ll.append(20)
+        ll.insert(25, 4)
+        self.assertEqual(ll.head.value, 5)
+        self.assertEqual(ll.tail.value, 25)
+        self.assertEqual(ll.length, 5)
+        self.assertEqual(ll.get(4).value, 25)
+
 
 if __name__ == "__main__":
     unittest.main()
