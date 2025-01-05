a841612edec6ad5b1b2cb7daa417c5e65e0a6884
Saurabh Verma <saurabh1088@gmail.com>
2025-01-05 15:46:38 +0530

    Added docstring for method find_middle_in_linked_list.

--- projects/data_structures/linked_list_programming.py
+++ projects/data_structures/linked_list_programming.py
@@ -13,6 +13,29 @@ logging.basicConfig(
 
 # Find the middle node of the linked list. If the list has an even number of elements, return the second middle node.
 def find_middle_in_linked_list(ll):
+    """
+    Find the middle node of a linked list.
+
+    This function traverses the linked list once to locate the middle node. 
+    If the list has an even number of nodes, it returns the node at the 
+    second of the two middle positions.
+
+    Args:
+        ll (LinkedList): An instance of the LinkedList class, representing 
+                         the linked list to search.
+
+    Returns:
+        Node: The middle node of the linked list, or None if the list is empty.
+              - If the list has one node, that node is returned.
+              - For lists with even length, the node at index length/2 is returned.
+
+    Time Complexity:
+        O(n), where n is the number of nodes in the list, since we traverse 
+        the list only once.
+
+    Space Complexity:
+        O(1), as we only use a single pointer 'temp' regardless of the list size.
+    """
     if ll.head is None:
         return None
     
