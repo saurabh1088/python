f27fbefbe8914018dd42b692f28cd13f6286a050
Saurabh Verma <saurabh1088@gmail.com>
2025-01-05 14:51:17 +0530

    Added linked_list_programming.py to add methods about problems over linked list with solutions.

---
+++ projects/data_structures/linked_list_programming.py
@@ -0,0 +1,13 @@
+import linked_list
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+
