9d386f5c253652a77ac72ecc25fe3961e3dd4a89
Saurabh Verma <saurabh1088@gmail.com>
2025-01-06 22:06:48 +0530

    Added test cases for linked list programming method.

--- projects/data_structures/linked_list_programming_tests.py
+++ projects/data_structures/linked_list_programming_tests.py
@@ -4,13 +4,38 @@ import linked_list
 
 class TestLinkedListProgramming(unittest.TestCase):
 
-    # Test cases for find_middle_in_linked_list when the linked list is empty
+    # Test case for find_middle_in_linked_list when the linked list is empty
     def test_find_middle_in_linked_list_empty(self):
         ll = linked_list.LinkedList(5)
         ll.head = None
         ll.tail = None
         ll.length = 0
         self.assertIsNone(llp.find_middle_in_linked_list(ll))
+
+    # Test case for find_middle_in_linked_list when the linked list has one node
+    def test_find_middle_in_linked_list_one_node(self):
+        ll = linked_list.LinkedList(5)
+        ll.append(10)
+        self.assertEqual(llp.find_middle_in_linked_list(ll).value, 10)
+
+    # Test case for find_middle_in_linked_list when the linked list has an odd number of nodes
+    def test_find_middle_in_linked_list_odd(self):
+        ll = linked_list.LinkedList(5)
+        ll.append(10)
+        ll.append(15)
+        ll.append(20)
+        ll.append(25)
+        self.assertEqual(llp.find_middle_in_linked_list(ll).value, 15)
+
+    # Test case for find_middle_in_linked_list when the linked list has an even number of nodes
+    def test_find_middle_in_linked_list_even(self):
+        ll = linked_list.LinkedList(5)
+        ll.append(10)
+        ll.append(15)
+        ll.append(20)
+        ll.append(25)
+        ll.append(30)
+        self.assertEqual(llp.find_middle_in_linked_list(ll).value, 20)
 
 
 if __name__ == "__main__":
