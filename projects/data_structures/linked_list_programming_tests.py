ebc2bfcf7a31cf3621e378776cad1e08151ebc07
Saurabh Verma <saurabh1088@gmail.com>
2025-01-07 18:57:35 +0530

    Added test case for is_circular_linked_list method in linked_list_programming.

--- projects/data_structures/linked_list_programming_tests.py
+++ projects/data_structures/linked_list_programming_tests.py
@@ -38,6 +38,39 @@ class TestLinkedListProgramming(unittest.TestCase):
         self.assertEqual(llp.find_middle_in_linked_list(ll).value, 20)
 
 
+    # Test case for is_circular_linked_list when the linked list is empty
+    def test_is_circular_linked_list_empty(self):
+        ll = linked_list.LinkedList(5)
+        ll.head = None
+        ll.tail = None
+        ll.length = 0
+        self.assertFalse(llp.is_circular_linked_list(ll))
+
+    # Test case for is_circular_linked_list when the linked list has one node
+    def test_is_circular_linked_list_one_node(self):
+        ll = linked_list.LinkedList(5)
+        ll.append(10)
+        self.assertFalse(llp.is_circular_linked_list(ll))
+
+    # Test case for is_circular_linked_list when the linked list is linear
+    def test_is_circular_linked_list_linear(self):
+        ll = linked_list.LinkedList(5)
+        ll.append(10)
+        ll.append(15)
+        ll.append(20)
+        ll.append(25)
+        self.assertFalse(llp.is_circular_linked_list(ll))
+
+    # Test case for is_circular_linked_list when the linked list is circular
+    def test_is_circular_linked_list_circular(self):
+        ll = linked_list.LinkedList(5)
+        ll.append(10)
+        ll.append(15)
+        ll.append(20)
+        ll.append(25)
+        ll.tail.next = ll.head
+        self.assertTrue(llp.is_circular_linked_list(ll))
+
 if __name__ == "__main__":
     unittest.main()
 
