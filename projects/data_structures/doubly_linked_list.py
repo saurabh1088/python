33897e2ba063b2d9d7a795e240428ac0e7c23c6f
Saurabh Verma <saurabh1088@gmail.com>
2025-01-08 08:00:47 +0530

    Added doubly_linked_list.py to add implementation of a doubly linked list.

---
+++ projects/data_structures/doubly_linked_list.py
@@ -0,0 +1,12 @@
+class Node:
+    
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    
+    def __init__(self):
+        self.head = None
