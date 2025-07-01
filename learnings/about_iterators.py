c89f0d8a8ff36c330724de562a42c7ffd8c41939
Saurabh Verma <saurabh1088@gmail.com>
2025-07-01 22:23:43 +0530

    Added a custom iterator.

--- learnings/about_iterators.py
+++ learnings/about_iterators.py
@@ -17,3 +17,28 @@ def example_iterator_behaviour():
     print(next(iterator))
     print(next(iterator))
     print(next(iterator))
+
+def example_iterator_behaviour_for_custom_counter_iterator():
+    counter = Counter(5)
+    iterator = iter(counter)
+    print(next(iterator))
+    print(next(iterator))
+    print(next(iterator))
+    print(next(iterator))
+    print(next(iterator))
+
+
+class Counter:
+    def __init__(self, maximum):
+        self.count = 0
+        self.maximum = maximum
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count < self.maximum:
+            self.count += 1
+            return self.count
+        else:
+            raise StopIteration
--- learnings/main.py
+++ learnings/main.py
@@ -40,6 +40,7 @@ def concepts_method_calls():
 
 def about_iterators_method_calls():
     about_iterators.example_iterator_behaviour()
+    about_iterators.example_iterator_behaviour_for_custom_counter_iterator()
 
 def main():
     logging.info('This is entry point')
