adce6d490d518c902836104d8f7733a2dce57b99
Saurabh Verma <saurabh1088@gmail.com>
2025-07-16 17:46:43 +0530

    Added big o notation example for o(1).

--- projects/data_structures/big_o_concepts.py
+++ projects/data_structures/big_o_concepts.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 def example_one(n):
     for i in range(n):
         print(i)
@@ -14,3 +17,13 @@ def example_three(n):
         for j in range(n):
             print(i,j)
 
+def example_big_o_one():
+    start_time = time.time()
+
+    large_list = list(range(10_000_000))
+    random_index = random.randint(0, len(large_list) - 1)
+    print(large_list[random_index])
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Execution Time: {elapsed_time:.6f} seconds")
--- projects/data_structures/main.py
+++ projects/data_structures/main.py
@@ -1,9 +1,12 @@
 import stack
+import big_o_concepts
+
+def big_o_examples():
+    big_o_concepts.example_big_o_one()
 
 def main():
     print("Hello, World!")
-    stack.example_stack_data_structure()
-
+    big_o_examples()
 
 if __name__ == '__main__':
     main()
