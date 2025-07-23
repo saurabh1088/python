ff3b3603ccff5926e422b862a51678581d8b85df
Saurabh Verma <saurabh1088@gmail.com>
2025-07-23 22:34:16 +0530

    Added comment for method example_dictionary_order.

--- learnings/about_dictionaries.py
+++ learnings/about_dictionaries.py
@@ -21,6 +21,7 @@ def example_dictionary_creation_constructor():
     logging.info(f"simple_dictionary with dict(): {simple_dictionary}")
     logging.info('----------------------------------------------')
 
+# Python dictionaries are ordered in Python 3.7 and later versions
 def example_dictionary_order():
     logging.info('--- Executing example_dictionary_order ---')
 
