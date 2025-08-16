127caebc542964204dadb1a473a6523fa4e77231
Saurabh Verma <saurabh1088@gmail.com>
2025-08-16 22:38:04 +0530

    Added example for creating simple lists.

--- learnings/about_lists.py
+++ learnings/about_lists.py
@@ -9,3 +9,27 @@ logging.basicConfig(
     ]
 )
 
+# --------------------------------------------------------------------------------
+# --- Example functions ---
+
+def example_lists_simple_concepts():
+    logging.info('--- Executing example_lists_simple_concepts ---')
+    logging.info('Creating a simple list')
+    simple_list_numbers = [1, 2, 3]
+    simple_list_strings = ['Batman', 'Superman', 'Wonder Woman']
+    logging.info(f'simple_list_numbers = {simple_list_numbers}')
+    logging.info(f'simple_list_strings = {simple_list_strings}')
+    logging.info('----------------------------------------------')
+
+
+# --------------------------------------------------------------------------------
+# --- Entry point ---
+
+def main():
+    logging.info('This is entry point for lists examples')
+    example_lists_simple_concepts()
+
+
+if __name__ == '__main__':
+    main()
+
