1f4fa99f7e372b1808d20485bea7965248092460
Saurabh Verma <saurabh1088@gmail.com>
2025-09-06 14:56:09 +0530

    Updated for practice seesion.

--- projects/sandbox/main.py
+++ projects/sandbox/main.py
@@ -11,23 +11,30 @@ logging.basicConfig(
 
 
 # --------------------------------------------------------------------------------
+# --- Practice Area ---
+def fizz_bizz():
+    logging.info("Starting FizzBuzz")
+    logging.info("Enter a number:")
+    number = int(input())
+    for i in range(1, number + 1):
+        logging.info(f'Value of i: {i}')
+        if i % 3 == 0 and i % 5 == 0:
+            logging.info("FizzBuzz")
+        elif i % 3 == 0:
+            logging.info("Fizz")
+        elif i % 5 == 0:
+            logging.info("Buzz")
+        else:
+            logging.info(i)
+    logging.info("FizzBuzz completed")
+
+
+# --------------------------------------------------------------------------------
 # --- Main Functionality ---
 def main():
     logging.info("Application started")
-    try:
-        # Simulate some operations
-        logging.debug("Performing operation 1")
-        # operation_1()
-        logging.debug("Operation 1 completed")
-
-        logging.debug("Performing operation 2")
-        # operation_2()
-        logging.debug("Operation 2 completed")
-
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-    finally:
-        logging.info("Application finished")
+    fizz_bizz()
+    logging.info("Application finished")
 
 
 if __name__ == "__main__":
