0b44794111a984fadeaa954779c2bb6be64e5333
Saurabh Verma <saurabh1088@gmail.com>
2025-09-17 16:40:18 +0530

    Practice session two.

--- projects/sandbox/server.py
+++ projects/sandbox/server.py
@@ -4,7 +4,20 @@ import socketserver
 PORT = 8000
 
 Handler = http.server.SimpleHTTPRequestHandler
+use_custom_handler = True
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at port {PORT}")
-    httpd.serve_forever()
+class CustomHandler(Handler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return super().do_GET()
+
+if __name__ == "__main__":
+    if use_custom_handler:
+        with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+            print(f"Serving at port {PORT}")
+            httpd.serve_forever()
+    else:
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            print(f"Serving at port {PORT}")
+            httpd.serve_forever()
