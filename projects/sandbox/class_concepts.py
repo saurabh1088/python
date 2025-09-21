13dd14bcfabd882421e7e9559d0fe62eb57929e8
Saurabh Verma <saurabh1088@gmail.com>
2025-09-21 21:18:18 +0530

    Practice session three.

--- projects/sandbox/class_concepts.py
+++ projects/sandbox/class_concepts.py
@@ -1,5 +1,8 @@
 class Vehicle:
+    class_variable = "I am class variable defined in Vehicle class"
+    
     def __init__(self):
+        self.instance_variable = "I am instance variable defined in Vehicle class"
         pass
     
     def start_engine(self):
@@ -48,8 +51,30 @@ def example_bike():
     bike = Bike()
     print(bike.start_engine())
     print(bike.stop_engine())
+    
+def class_variables_playground():
+    print(f"Vehicle class variable : {Vehicle.class_variable}")
+    print(f"Car class variable : {Car.class_variable}")
+    print(f"Bike class variable : {Bike.class_variable}")
+    Vehicle.class_variable = "I am modified class variable"
+    print("After modifying Vehicle.class_variable")
+    print(f"Vehicle class variable : {Vehicle.class_variable}")
+    print(f"Car class variable : {Car.class_variable}")
+    print(f"Bike class variable : {Bike.class_variable}")
+
+def instance_variables_playground():
+    vehicle = Vehicle()
+    car = Car()
+    bike = Bike()
+    print(f"Vehicle instance variable : {vehicle.instance_variable}")
+    print(f"Car instance variable : {car.instance_variable}")
+    print(f"Bike instance variable : {bike.instance_variable}")
+    vehicle.instance_variable = "I am modified instance variable in vehicle object"
+    print("After modifying vehicle.instance_variable")
+    print(f"Vehicle instance variable : {vehicle.instance_variable}")
+    print(f"Car instance variable : {car.instance_variable}")
+    print(f"Bike instance variable : {bike.instance_variable}")
+
 
 if __name__ == "__main__":
-    example_vehicle()
-    example_car()
-    example_bike()
+    instance_variables_playground()
