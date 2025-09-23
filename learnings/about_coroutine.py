d71459e22a3fa04a09615e31b714ad1e26086fb5
Saurabh Verma <saurabh1088@gmail.com>
2025-09-23 18:53:19 +0530

    Added basic example for setting base for concept.

--- learnings/about_coroutine.py
+++ learnings/about_coroutine.py
@@ -1,1 +1,12 @@
 import asyncio
+
+async def an_async_function_showing_coroutine():
+    print("This is an async function")
+    print("Simulating some async work with asyncio.sleep")
+    print("Sleeping...")
+    await asyncio.sleep(10)
+    print("Waking up...")
+    print("Async function is done")
+
+if __name__ == "__main__":
+    asyncio.run(an_async_function_showing_coroutine())
