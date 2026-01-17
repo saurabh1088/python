c6f61f20347937cdad26eac49cd2e3c58de1ff09
Saurabh Verma <saurabh1088@gmail.com>
2026-01-17 10:49:47 +0530

    Updated main.py file and added db files.

--- projects/sqlite_playground/main.py
+++ projects/sqlite_playground/main.py
@@ -1,26 +1,27 @@
 import sqlite3
 
-conn = sqlite3.connect("mydb.sqlite")
+conn = sqlite3.connect("mydb_v1.sqlite")
 cursor = conn.cursor()
 
 # Create the users table if it doesn't exist
 cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
+    CREATE TABLE IF NOT EXISTS employee (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        email TEXT
+        email TEXT,
+        designation TEXT
     )
 """)
 
 # Insert some sample data (optional)
-cursor.execute("INSERT OR IGNORE INTO users (name, email) VALUES (?, ?)", ("John Doe", "john@example.com"))
-cursor.execute("INSERT OR IGNORE INTO users (name, email) VALUES (?, ?)", ("Jane Smith", "jane@example.com"))
+cursor.execute("INSERT OR IGNORE INTO employee (name, email, designation) VALUES (?, ?, ?)", ("John Doe", "john@example.com", "CEO"))
+cursor.execute("INSERT OR IGNORE INTO employee (name, email, designation) VALUES (?, ?, ?)", ("Jane Smith", "jane@example.com", "CTO"))
 
 # Commit the changes
 conn.commit()
 
 # Now query the users table
-cursor.execute("SELECT * FROM users")
+cursor.execute("SELECT * FROM employee")
 print(cursor.fetchall())
 
 conn.close()
