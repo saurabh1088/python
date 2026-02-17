be84157348a32fbbcb154e97484199f6443465f2
Saurabh Verma <saurabh1088@gmail.com>
2026-02-17 18:30:27 +0530

    Python - Immutable Example - Session One - 17th February 2026

--- learnings/about_init_and_new.py
+++ learnings/about_init_and_new.py
@@ -11,6 +11,30 @@ class Employee:
         self.name = name
         self.id = id
 
+
+class ImmutablePoint:
+    # TODO: Check if __slots__ is really necessary here for immutability, or if we can achieve immutability without it.
+    __slots__ = ('x', 'y')
+
+    def __new__(cls, x, y):
+        instance = super().__new__(cls)
+        super().__setattr__(instance, 'x', x)
+        super().__setattr__(instance, 'y', y)
+        return instance
+    
+    def __setattr__(self, key, value):
+        raise AttributeError("ImmutablePoint is immutable")
+    
+    def __init__(self, x, y):
+        pass
+
+
 # Creating an instance of Employee
 employee1 = Employee("John Doe", 12345)
-    
+point1 = ImmutablePoint(10, 20)
+print(point1.x)  # Output: 10
+print(point1.y)  # Output: 20
+print("Attempting to modify point1's x attribute...")
+point1.x = 30  # This will work, but it is not recommended to modify attributes of an immutable class
+print(point1.x)  # Output: 30
+
