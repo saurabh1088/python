3744001e32c66671d4e9e6627235f1b2d620f728
Saurabh Verma <saurabh1088@gmail.com>
2026-03-02 22:25:49 +0530

    Added example of block scope behaviour in Python.

--- learnings/about_legb.py
+++ learnings/about_legb.py
@@ -11,4 +11,20 @@ def example_function():
         print(len(LOCAL)) # Here len is a built-in function, so it will look in the built-in scope
     inner_function()
 
+
+# This will work because Python does not have block scope.
+# Python has function scope. So BLOCK_VAR is accessible outside the if block.
+def block_scope_example_using_if():
+    if True:
+        IF_BLOCK_VAR = "If Block Variable"
+    print(IF_BLOCK_VAR)
+
+def block_scope_example_using_for():
+    for i in range(1):
+        FOR_BLOCK_VAR = "For Block Variable"
+    print(FOR_BLOCK_VAR)
+
 example_function()
+block_scope_example_using_if()
+block_scope_example_using_for()
+
